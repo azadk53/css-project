9b0f06fa6b3cbc395faeed3acc9de3940993b17b
azadk53 <mistazad@gmail.com>
2022-03-13 12:50:10 +0100

    header course campus

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const navLinks = document.getElementById('nav-links')
+function showMenubar() {
+    navLinks.style.right = "0";
+}
+function hideMenubar() {
+    navLinks.style.right = "-200px";
+}
